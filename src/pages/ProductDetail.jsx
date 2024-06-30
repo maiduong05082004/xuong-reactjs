34889c03160c0de70f721b4b338c45351ef4d920
maiduong <duongmdph40323@gmail.com>
2024-06-30 08:09:38 +0700

    update product detail

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -4,22 +4,29 @@ import instance from "../axios";
 
 const ProductDetail = () => {
 	const { id } = useParams();
-	const [p, setP] = useState({});
+	const [product, setProduct] = useState({});
+
 	useEffect(() => {
 		(async () => {
 			try {
 				const { data } = await instance.get(`/products/${id}`);
-				setP(data);
+				setProduct(data);
 			} catch (error) {
 				console.log(error);
 			}
 		})();
-	}, []);
+	}, [id]);
+
 	return (
 		<div>
 			<h1>Product Detail</h1>
-			<img src={p.thumbnail} alt="" />
-			<h1>{p.title}</h1>
+			{product.thumbnail && <img src={product.thumbnail} alt={product.title} />}
+			<h1>{product.title}</h1>
+			<p>{product.description}</p>
+			<p>Price: ${product.price}</p>
+			<p>Stock: {product.stock}</p>
+			<p>Rating: {product.rating}</p>
+			<p>Brand: {product.brand}</p>
 		</div>
 	);
 };
